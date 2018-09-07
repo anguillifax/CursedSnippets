b101bc17a0b98a6739f6284e18a32eba701b8929
atomicdiamond <atomic.diamond2000@gmail.com>
2018-09-07 10:11:07 -0700

    Init Commit

---
+++ CPlusPlus.cpp
@@ -0,0 +1,16 @@
+#define MY_AMAZING_MACRO(x) x
+
+using namespace std;
+
+
+void Function() {
+
+	const char* str = "string";
+	str = &(*str);
+
+
+
+
+
+}
+
---
+++ CSharp.cs
@@ -0,0 +1,66 @@
+/////////////////
+// C# EXAMPLES //
+/////////////////
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+public class Code {
+
+	void Function() {
+
+		try {
+			//...
+		} catch (Exception e) {
+			throw e;
+		}
+
+
+
+		object[] objs = new object[1];
+		foreach (var item in objs) {
+			//...
+		}
+
+
+
+		int tern = true ? 1 : 0;
+
+
+
+		object ob = new object();
+		ob = ob as object;
+		Console.WriteLine((null ?? ob).ToString());
+
+
+
+		for (; false;) {
+			//...
+		}
+
+
+
+		switch (true) {
+			default:
+				//...
+		}
+
+
+
+		IEnumerable<int> collection;
+		collection = collection.Select((x) => true);
+
+
+
+		
+
+
+	}
+
+	public override string ToString() {
+		return base.ToString() + "";
+	}
+
+
+}
