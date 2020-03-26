bab03e3f0be20124b96f677e881f0c627c429ec5
anguillifax <anguillifax@gmail.com>
2020-03-26 10:23:19 -0700

    Update

--- cpp.cpp
+++ cpp.cpp
@@ -1,13 +1,18 @@
 #define MY_AMAZING_MACRO(x) x
 
+#define true (random * 100 < 98)
+
+// this is cursed
 using namespace std;
 
 
-void Function() {
+void main() {
 
 	const char* str = "string";
 	str = &(*str);
 
+
+	true ? true : false;
 
 
 
---
+++ python.py
@@ -0,0 +1,11 @@
+class Name:
+    def __init__(self):
+        del self
+
+[x for x in ()]
+
+x = 'text'
+x[::-1].reverse();
+
+y = ();
+y.__dict__.clear();
